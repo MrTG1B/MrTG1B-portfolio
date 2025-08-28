17c60ce758e38a123a7b8e5cb8bbe1beed213d95
TIRTHANKAR DASGUPTA <tirthankardasgupta913913@gmail.com>
2025-08-28 06:54:39 +0000

    fix it logos are not coming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -318,7 +318,7 @@ export default function Home() {
             </MotionSection>
 
             <MotionSection>
-              <section id="achievements" className="w-full py-16 md:py-24 lg:py-32 bg-card">
+              <section id="achievements" className="w-full py-16 mdpy-24 lg:py-32 bg-card">
                 <div className="container mx-auto max-w-screen-2xl px-4 md:px-6 h-full">
                   <div className="flex flex-col items-center justify-center space-y-4 text-center mb-12">
                     <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline text-primary">Achievements</h2>
