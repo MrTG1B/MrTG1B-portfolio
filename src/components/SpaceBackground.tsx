960b5ea2f1e2c83d49c463ceaf2e25b69ce6a59f
Tirthankar Dasgupta <tirthankardasgupta913913@gmail.com>
2025-11-22 14:35:18 +0530

    check

--- src/components/SpaceBackground.tsx
+++ src/components/SpaceBackground.tsx
@@ -5,20 +5,20 @@ import React from "react";
 
 export default function SpaceBackground() {
   return (
-    <div className="space-bg pointer-events-none fixed inset-0 -z-10">
+    <div className="space-bg pointer-events-none fixed inset-0 z-0">
       <div className="stars" aria-hidden />
 
       <div className="space-objects">
         <div className="planet" aria-hidden>
-          <Image src="/Images/planet.svg" alt="planet" width={220} height={220} />
+          <Image src="/Images/planet.svg" alt="planet" width={220} height={220} priority />
         </div>
 
         <div className="satellite" aria-hidden>
-          <Image src="/Images/satellite.svg" alt="satellite" width={120} height={120} />
+          <Image src="/Images/satellite.svg" alt="satellite" width={120} height={120} priority />
         </div>
 
         <div className="rocket" aria-hidden>
-          <Image src="/Images/rocket.svg" alt="rocket" width={140} height={140} />
+          <Image src="/Images/rocket.svg" alt="rocket" width={140} height={140} priority />
         </div>
       </div>
     </div>
